27f78d3857a416e24a5e070ee482d9818d6eb9d8
jeremy.gau <caliban3010@gmail.com>
2020-04-07 17:24:54 +0200

    renommage de rules

--- Grammar.java
+++ Grammar.java
@@ -1,93 +1,81 @@
 import java.util.*;
 
 public class Grammar {
- private NonTerminal axiom;
- private HashMap<NonTerminal, ArrayList<RightHandSide>> Rules;
+    private NonTerminal axiom;
+    private HashMap<NonTerminal, ArrayList<RightHandSide>> rules;
 
-
- /**
-  * construit une grammaire d'axiome ax et sans règle de production
-  */
- public Grammar(NonTerminal ax) {
-    this.axiom = ax;
-    this.Rules = new HashMap<>();
- }
+    /**
+     * construit une grammaire d'axiome ax et sans règle de production
+     */
+    public Grammar(NonTerminal ax) {
+        this.axiom = ax;
+        this.rules = new HashMap<>();
+    }
 
     /**
      * ajoute une nouvelle règle de production à la grammaire avec n pour membre gauche et r pout membre droit
      */
-/* public void addRule(NonTerminal n, RightHandSide r) {
-     ArrayList<RightHandSide> RhsList;
-     if (Rules.containsKey(n)) {
-         RhsList = Rules.get(n);
-         Rules.remove(n);
-     } else {
-         RhsList = new ArrayList<RightHandSide>();
-     }
-     RhsList.add(r);
-     Rules.put(n, RhsList);
-     }*/
 
     public void addRule(NonTerminal n, RightHandSide r) {
-        if (Rules.containsKey(n)) {
-            Rules.get(n).add(r);
+        if (rules.containsKey(n)) {
+            rules.get(n).add(r);
         } else {
             ArrayList<RightHandSide> RhsList = new ArrayList<>();
             RhsList.add(r);
-            Rules.put(n, RhsList);
+            rules.put(n, RhsList);
         }
     }
 
-
     /**
      * retourne l'axiome de la grammaire
+     *
      * @return le NonTerminal axiome de la grammaire
      */
-    public NonTerminal getAxiom(){
+    public NonTerminal getAxiom() {
         return axiom;
     }
-
 
 
     /**
      * modifie l'axiome de la grammaire
      */
-    public void setAxiom(NonTerminal n){
-     axiom = n;
- }
+    public void setAxiom(NonTerminal n) {
+        axiom = n;
+    }
 
 
-
- /**
-  * teste si un non-terminal peut s'effacer, c'est-à-dire se dériver en epsilon
-  * @return vrai (true) si le non-terminal peut se dériver en epsilon
-  */
- public Boolean ErasableNonTerminal(NonTerminal N){
-     for (RightHandSide rightHandSide : Rules.get(N)) {
-        if (ErasableRhs(rightHandSide)) return true;
-     }
-     return false;
- }
+    /**
+     * teste si un non-terminal peut s'effacer, c'est-à-dire se dériver en epsilon
+     *
+     * @return vrai (true) si le non-terminal peut se dériver en epsilon
+     */
+    public Boolean ErasableNonTerminal(NonTerminal N) {
+        for (RightHandSide rightHandSide : rules.get(N)) {
+            if (ErasableRhs(rightHandSide)) return true;
+        }
+        return false;
+    }
 
     /**
      * teste si une suite de Symbol formant un membre droit peut s'effacer, c'est-à-dire se dériver en epsilon
+     *
      * @return vrai (true) si un membre droit peut se dériver en epsilon
      */
- public Boolean ErasableRhs(RightHandSide r){
-     for (Symbol symbol : r.getSymbols()) {
-         if (symbol instanceof Terminal || !ErasableNonTerminal((NonTerminal)symbol))
-             return false;
-     }
-    return true;
+    public Boolean ErasableRhs(RightHandSide r) {
+        for (Symbol symbol : r.getSymbols()) {
+            if (symbol instanceof Terminal || !ErasableNonTerminal((NonTerminal) symbol))
+                return false;
+        }
+        return true;
 
 
- }
+    }
 
     public String toString() {
         StringBuilder grammarString = new StringBuilder();
-        for (NonTerminal nonTerminal : Rules.keySet()){
+        for (NonTerminal nonTerminal : rules.keySet()) {
             grammarString.append(nonTerminal).append(" -> ");
-            for (RightHandSide rightHandSide : Rules.get(nonTerminal)){
+            for (RightHandSide rightHandSide : rules.get(nonTerminal)) {
                 grammarString.append(rightHandSide).append(" | ");
             }
             grammarString.replace(grammarString.length() - 3, grammarString.length(), "\n");
@@ -96,9 +84,7 @@ public class Grammar {
     }
 
 
-
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         String output;
 
         Terminal ta = new Terminal("a");
@@ -132,11 +118,11 @@ public class Grammar {
 
 
         Grammar g = new Grammar(s);
-        g.addRule(s,r);
-        g.addRule(s,rb);
-        g.addRule(s,rc);
+        g.addRule(s, r);
+        g.addRule(s, rb);
+        g.addRule(s, rc);
 
-        g.addRule(a,rd);
+        g.addRule(a, rd);
 
         g.addRule(t, r1);
         g.addRule(t, r2);
@@ -145,12 +131,8 @@ public class Grammar {
 
         System.out.println(output);
 
-        for (NonTerminal nonterminal : g.Rules.keySet()) {
+        for (NonTerminal nonterminal : g.rules.keySet()) {
             System.out.println(nonterminal + " is Erasable ? " + g.ErasableNonTerminal(nonterminal));
         }
-
     }
-
-
-
 }
